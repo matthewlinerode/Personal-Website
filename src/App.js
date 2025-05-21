c367a43fb528964612025cc9e467694454b0ca35
matthewline <your-email@example.com>
2025-05-21 17:16:58 -0400

    Small test to explore deployment on netlify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+function handleClick() {
+  alert("Button clicked!");
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" /> 
+        <button onClick={handleClick}>Click Here!</button>
       </header>
     </div>
   );
